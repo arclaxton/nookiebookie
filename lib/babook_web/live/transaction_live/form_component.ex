800d30f78bd2d88fba73f2a3639b2de3c1622ee1
arclaxton <arclaxton@pm.me>
2023-03-17 15:30:20 -0500

    tabs

--- lib/babook_web/live/transaction_live/form_component.ex
+++ lib/babook_web/live/transaction_live/form_component.ex
@@ -1,55 +1,55 @@
 defmodule BabookWeb.TransactionLive.FormComponent do
-  use BabookWeb, :live_component
+	use BabookWeb, :live_component
 
-  alias Babook.Store
+	alias Babook.Store
 
-  @impl true
-  def update(%{transaction: transaction} = assigns, socket) do
-    changeset = Store.change_transaction(transaction)
+	@impl true
+	def update(%{transaction: transaction} = assigns, socket) do
+		changeset = Store.change_transaction(transaction)
 
-    {:ok,
-     socket
-     |> assign(assigns)
-     |> assign(:changeset, changeset)}
-  end
+		{:ok,
+		 socket
+		 |> assign(assigns)
+		 |> assign(:changeset, changeset)}
+	end
 
-  @impl true
-  def handle_event("validate", %{"transaction" => transaction_params}, socket) do
-    changeset =
-      socket.assigns.transaction
-      |> Store.change_transaction(transaction_params)
-      |> Map.put(:action, :validate)
+	@impl true
+	def handle_event("validate", %{"transaction" => transaction_params}, socket) do
+		changeset =
+			socket.assigns.transaction
+			|> Store.change_transaction(transaction_params)
+			|> Map.put(:action, :validate)
 
-    {:noreply, assign(socket, :changeset, changeset)}
-  end
+		{:noreply, assign(socket, :changeset, changeset)}
+	end
 
-  def handle_event("save", %{"transaction" => transaction_params}, socket) do
-    save_transaction(socket, socket.assigns.action, transaction_params)
-  end
+	def handle_event("save", %{"transaction" => transaction_params}, socket) do
+		save_transaction(socket, socket.assigns.action, transaction_params)
+	end
 
-  defp save_transaction(socket, :edit, transaction_params) do
-    case Store.update_transaction(socket.assigns.transaction, transaction_params) do
-      {:ok, _transaction} ->
-        {:noreply,
-         socket
-         |> put_flash(:info, "Transaction updated successfully")
-         |> push_redirect(to: socket.assigns.return_to)}
+	defp save_transaction(socket, :edit, transaction_params) do
+		case Store.update_transaction(socket.assigns.transaction, transaction_params) do
+			{:ok, _transaction} ->
+				{:noreply,
+				 socket
+				 |> put_flash(:info, "Transaction updated successfully")
+				 |> push_redirect(to: socket.assigns.return_to)}
 
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, :changeset, changeset)}
-    end
-  end
+			{:error, %Ecto.Changeset{} = changeset} ->
+				{:noreply, assign(socket, :changeset, changeset)}
+		end
+	end
 
-  defp save_transaction(socket, :new, transaction_params) do
-    case Store.create_transaction(transaction_params) do
-      {:ok, _transaction} ->
-        {:noreply,
-         socket
-         |> put_flash(:info, "Transaction created successfully")
-         |> push_redirect(to: socket.assigns.return_to)}
+	defp save_transaction(socket, :new, transaction_params) do
+		case Store.create_transaction(transaction_params) do
+			{:ok, _transaction} ->
+				{:noreply,
+				 socket
+				 |> put_flash(:info, "Transaction created successfully")
+				 |> push_redirect(to: socket.assigns.return_to)}
 
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, changeset: changeset)}
-    end
-  end
+			{:error, %Ecto.Changeset{} = changeset} ->
+				{:noreply, assign(socket, changeset: changeset)}
+		end
+	end
 end
