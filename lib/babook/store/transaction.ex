bef308c2911bb90c4cd65db335028b2cd8010a92
arclaxton <arclaxton@pm.me>
2023-03-20 12:11:45 -0500

    tabs

--- lib/babook/store/transaction.ex
+++ lib/babook/store/transaction.ex
@@ -1,24 +1,24 @@
 defmodule Babook.Store.Transaction do
-  use Ecto.Schema
-  import Ecto.Changeset
+	use Ecto.Schema
+	import Ecto.Changeset
 
-  @primary_key {:id, :binary_id, autogenerate: true}
-  @foreign_key_type :binary_id
-  schema "transactions" do
-    field :account, :integer
-    field :amount, :float
-    field :budget_allocation, :integer
-    field :category, :string
-    field :merchant, :string
-    field :transfer, :boolean, default: false
+	@primary_key {:id, :binary_id, autogenerate: true}
+	@foreign_key_type :binary_id
+	schema "transactions" do
+		field :account, :integer
+		field :amount, :float
+		field :budget_allocation, :integer
+		field :category, :string
+		field :merchant, :string
+		field :transfer, :boolean, default: false
 
-    timestamps()
-  end
+		timestamps()
+	end
 
-  @doc false
-  def changeset(transaction, attrs) do
-    transaction
-    |> cast(attrs, [:account, :amount, :transfer, :merchant, :category, :budget_allocation])
-    |> validate_required([:account, :amount, :transfer, :merchant, :category, :budget_allocation])
-  end
+	@doc false
+	def changeset(transaction, attrs) do
+		transaction
+		|> cast(attrs, [:account, :amount, :transfer, :merchant, :category, :budget_allocation])
+		|> validate_required([:account, :amount, :transfer, :merchant, :category, :budget_allocation])
+	end
 end
