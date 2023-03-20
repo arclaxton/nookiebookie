591b8d4d1851d0b0d53af8f5624831a8cc6b781d
arclaxton <arclaxton@pm.me>
2023-03-20 12:44:39 -0500

    seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,46 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Babook.Store.Transaction
+
+Babook.Repo.insert! %Transaction{
+	account: 0,
+	amount: 20.0,
+	budget_allocation: 0,
+	category: "category",
+	merchant: "merchant",
+	transfer: false
+}
+Babook.Repo.insert! %Transaction{
+	account: 0,
+	amount: 20.0,
+	budget_allocation: 0,
+	category: "category",
+	merchant: "merchant",
+	transfer: false
+}
+Babook.Repo.insert! %Transaction{
+	account: 0,
+	amount: 20.0,
+	budget_allocation: 0,
+	category: "category",
+	merchant: "merchant",
+	transfer: false
+}
+Babook.Repo.insert! %Transaction{
+	account: 0,
+	amount: 20.0,
+	budget_allocation: 0,
+	category: "category",
+	merchant: "merchant",
+	transfer: false
+}
+Babook.Repo.insert! %Transaction{
+	account: 0,
+	amount: 20.0,
+	budget_allocation: 0,
+	category: "category",
+	merchant: "merchant",
+	transfer: false
+}
