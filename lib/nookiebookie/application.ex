414ac550e18585888682fe50e6a1b099f86502c9
arclaxton <arclaxton@pm.me>
2023-07-13 21:51:35 -0500

    tabs

--- lib/nookiebookie/application.ex
+++ lib/nookiebookie/application.ex
@@ -1,36 +1,37 @@
 defmodule NookieBookie.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
+	# See https://hexdocs.pm/elixir/Application.html
+	# for more information on OTP Applications
+	@moduledoc false
 
-  use Application
+	use Application
 
-  @impl true
-  def start(_type, _args) do
-    children = [
-      # Start the Ecto repository
-      NookieBookie.Repo,
-      # Start the Telemetry supervisor
-      NookieBookieWeb.Telemetry,
-      # Start the PubSub system
-      {Phoenix.PubSub, name: NookieBookie.PubSub},
-      # Start the Endpoint (http/https)
-      NookieBookieWeb.Endpoint
-      # Start a worker by calling: NookieBookie.Worker.start_link(arg)
-      # {NookieBookie.Worker, arg}
-    ]
+	@impl true
+	def start(_type, _args) do
+		children = [
+			# Start the Ecto repository
+			NookieBookie.Repo,
+			# Start the Telemetry supervisor
+			NookieBookieWeb.Telemetry,
+			# Start the PubSub system
+			{Phoenix.PubSub, name: NookieBookie.PubSub},
+			# Start the Endpoint (http/https)
+			NookieBookieWeb.Endpoint,
+			# Start a worker by calling: NookieBookie.Worker.start_link(arg)
+			# {NookieBookie.Worker, arg}
+			# NookieBookie.PlaidServer
+		]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: NookieBookie.Supervisor]
-    Supervisor.start_link(children, opts)
-  end
+		# See https://hexdocs.pm/elixir/Supervisor.html
+		# for other strategies and supported options
+		opts = [strategy: :one_for_one, name: NookieBookie.Supervisor]
+		Supervisor.start_link(children, opts)
+	end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
-  @impl true
-  def config_change(changed, _new, removed) do
-    NookieBookieWeb.Endpoint.config_change(changed, removed)
-    :ok
-  end
+	# Tell Phoenix to update the endpoint configuration
+	# whenever the application is updated.
+	@impl true
+	def config_change(changed, _new, removed) do
+		NookieBookieWeb.Endpoint.config_change(changed, removed)
+		:ok
+	end
 end
